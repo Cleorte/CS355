db38623483dbb89818ff22c5eb61a6bdbc22d401
Cleorte <Cleortefang@gmail.com>
2023-02-07 18:25:25 -0500

    a2

---
+++ todo/script.js
@@ -0,0 +1,17 @@
+
+
+const mainDiv = document.getElementById("main");
+const todoTextInput = document.getElementById("todoContent");
+const todoAddButton = document.getElementById("todoButton");
+
+function addClickableDiv(place){
+    var d = document.createElement('div');
+    mainDiv.appendChild(d);
+    d.innerHTML = todoTextInput.value;
+    d.addEventListener('click', function(){
+        d.classList.add ( 'completed') ;
+    });
+    
+}
+
+todoAddButton.addEventListener('click', addClickableDiv);
